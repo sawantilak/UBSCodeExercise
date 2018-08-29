642c7bd2cf6b655583323379ad30484bf46ec237
Sawan Tilak Kuntala <sawantilak@gmail.com>
2018-08-29 15:19:27 +0530

    Removing the function that's not needed.

--- src/main/java/com/ubs/NegativeNumberException.java
+++ src/main/java/com/ubs/NegativeNumberException.java
@@ -5,9 +5,4 @@ public class NegativeNumberException extends Exception {
     public NegativeNumberException(String message) {
         super();
     }
-
-    public String toString() {
-
-        return super.toString();
-    }
 }
